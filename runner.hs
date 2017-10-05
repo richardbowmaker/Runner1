fd62043ae6335d56bb198950391f472af1e57793
richardbowmaker <rjb.accounts@live.co.uk>
2017-10-05 19:14:42 +0100

    history added

--- runner.hs
+++ runner.hs
@@ -3,8 +3,8 @@
 data Point = Point {x :: Int, y :: Int} deriving (Eq,Show)
 data Runner = Runner {name :: String, direction :: Point, velocity :: Int} deriving (Eq,Show)
 data RunnerState = RunnerState {runner :: Runner, position :: Point } deriving (Eq,Show)
-data GameState = GameState { time :: Int, runners :: [RunnerState]} deriving (Eq,Show)
-data GameHistory = GameHistory [GameState]
+data GameState = GameState {time :: Int, runners :: [RunnerState]} deriving (Eq,Show)
+data GameHistory = GameHistory {history :: [GameState]} deriving (Eq,Show)
  
 r1 = Runner "one" (Point 1 0) 2
 r2 = Runner "one" (Point 0 1) 3
@@ -14,14 +14,14 @@ rs = [r1, r2]
 rss = map (\r -> (RunnerState r (Point 0 0))) rs 
  
  -- Length of run, list of players
-doRun :: Int -> [Runner] -> GameState
-doRun endTime rs = doRun' endTime (GameState 0 (map (\r -> (RunnerState r (Point 0 0))) rs))
+doRun :: Int -> [Runner] -> GameHistory
+doRun endTime rs = doRun' endTime (GameState 0 (map (\r -> (RunnerState r (Point 0 0))) rs)) (GameHistory [])
  
  -- length of run,
-doRun' :: Int -> GameState -> GameState
-doRun' endTime gs@(GameState time rs)
-    | time == endTime = gs
-    | otherwise = doRun' endTime (GameState (time + 1) (map (\r -> updateRunnerState time r) rs))
+doRun' :: Int -> GameState -> GameHistory -> GameHistory
+doRun' endTime state@(GameState time rs) (GameHistory history)
+    | time == endTime = (GameHistory (history ++ [state]))
+    | otherwise = doRun' endTime (GameState (time + 1) (map (\r -> updateRunnerState time r) rs)) (GameHistory (history ++ [state]))
     
 updateRunnerState :: Int -> RunnerState -> RunnerState
 updateRunnerState time rs@(RunnerState r@(Runner _ (Point incX incY ) velocity) (Point atX atY)) 
